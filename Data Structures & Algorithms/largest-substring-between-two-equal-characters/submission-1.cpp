5d8e994fb26d753c1a638df3883538f0dd4d92b3
Rahul Samanta <145849509+rahulsamanta82@users.noreply.github.com>
2026-06-27 20:23:44 +0530

    Add: largest-substring-between-two-equal-characters - submission-1

---
+++ Data Structures & Algorithms/largest-substring-between-two-equal-characters/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char, int> mp;
+        int ans = -1;
+        for(int i=0; i<s.size(); i++){
+            if(mp.find(s[i])==mp.end()){
+                mp[s[i]] = i;
+
+            }
+            else{
+                ans=max(ans, i-mp[s[i]] - 1);
+            }
+        }
+        return ans;
+
+
+        // unordered_map<char, int> mp;
+        // int ans = -1;
+        
+
+        // for (int i = 0; i < s.size(); i++) {
+
+        //     if (mp.find(s[i]) == mp.end()) {
+        //         mp[s[i]] = i;
+        //     }
+        //     else {
+        //         ans = max(ans, i - mp[s[i]] - 1);
+        //     }
+        // }
+
+        // return ans;
+    }
+};
